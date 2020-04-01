edc1239ae7f93777537eb2bff8905d5464cddd55
rockts <rockts@sina.com>
2020-04-01 16:27:43 +0800

    定义服务方法

--- src/posts/provideres/demo/demo.service.ts
+++ src/posts/provideres/demo/demo.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from '../../interfaces/post.interface';
 
 @Injectable()
-export class DemoService {}
+export class DemoService {
+  private readonly posts: Post[] = [];
+
+  findAll(): Post[] {
+    return this.posts;
+  }
+
+  create(post: Post) {
+    this.posts.push(post);
+  }
+}
