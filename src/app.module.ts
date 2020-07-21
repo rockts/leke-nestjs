7a1cb3484bd9995d0db09d911f37da96800ae3d2
rockts <rockts@sina.com>
2020-07-21 12:51:38 +0800

    数据库连接配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3308,
+      username: 'nest',
+      password: 'password',
+      database: 'nest',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.ts'],
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
