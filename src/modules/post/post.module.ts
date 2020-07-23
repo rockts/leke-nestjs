50f6bbb55534b0a6a3b59cd8f71c11d21422fd02
rockts <rockts@sina.com>
2020-07-24 03:51:01 +0800

    使用实体的 Repository 存储数据

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './post.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
