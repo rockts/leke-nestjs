4d22f49b5fb5e42488c14fc78e41629e6b3fcd41
rockts <rockts@sina.com>
2020-07-24 06:37:21 +0800

    策略（Strategy）

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport';
+import { ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { JwtPayload } from '../auth.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'o2Vqc2MY7pa0p2nzFzRvWUMFWkoVhLcI',
+    });
+  }
+
+  async validate(payload: JwtPayload, done: VerifiedCallback) {
+    console.log('payload', payload);
+  }
+}
