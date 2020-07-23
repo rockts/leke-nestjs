f4f6471beddb41c3426b4207adcca5c0ac8423b8
rockts <rockts@sina.com>
2020-07-24 04:30:19 +0800

    用户实体（User Entity）

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
