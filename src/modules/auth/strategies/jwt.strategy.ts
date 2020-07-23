92ca52ba6bbdf387aaad37ad841c5450e59ee343
rockts <rockts@sina.com>
2020-07-24 06:55:54 +0800

    为路由启用验证用户身份（AuthGuard）

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport';
-import { ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
 import { JwtPayload } from '../auth.interface';
 
 @Injectable()
