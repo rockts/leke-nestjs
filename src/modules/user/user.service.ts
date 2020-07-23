7092ed69a67e4d29bdbb1ffb763ebe2f00747957
rockts <rockts@sina.com>
2020-07-24 04:54:31 +0800

    查询单个用户数据

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -21,6 +25,16 @@ export class UserService {
 
     const entity = await this.userRepository.create(data);
     await this.userRepository.save(entity);
+    return entity;
+  }
+
+  async show(id: string) {
+    const entity = await this.userRepository.findOne(id);
+
+    if (!entity) {
+      throw new NotFoundException('没找到用户。');
+    }
+
     return entity;
   }
 }
