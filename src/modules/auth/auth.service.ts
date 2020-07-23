cde91707e107f5dfd2f051f2bc4a7bd0f3931afd
rockts <rockts@sina.com>
2020-07-24 06:25:45 +0800

    签发 JWT（jwtService.sign）

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginDto } from './auth.dto';
+import { JwtPayload } from './auth.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(data: LoginDto) {
     const { name, password } = data;
@@ -18,6 +23,17 @@ export class AuthService {
       throw new UnauthorizedException('密码不匹配。');
     }
 
-    return entity;
+    const { id } = entity;
+    const payload = { id, name };
+    const token = this.signToken(payload);
+
+    return {
+      ...payload,
+      token,
+    };
+  }
+
+  signToken(data: JwtPayload) {
+    return this.jwtService.sign(data);
   }
 }
