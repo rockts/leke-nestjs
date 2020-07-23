68a080d830f6b0794c5817034a298f0261ec511f
rockts <rockts@sina.com>
2020-07-24 05:50:07 +0800

    创建身份验证模块（Auth）

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PostModule, UserModule],
+  imports: [TypeOrmModule.forRoot(), PostModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
